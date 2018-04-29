65c37dde34d0757d1ec2c17f9c35f553c67931bf
trepix <pau.trepat.segura@gmail.com>
2018-04-29 04:06:13 +0200

    #6 Add feature to generate the cheat-sheet of numbers

---
+++ src/main/java/io/trepixxx/fizzbuzz/CheatSheetGenerator.java
@@ -0,0 +1,13 @@
+package io.trepixxx.fizzbuzz;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class CheatSheetGenerator {
+    static void print() {
+        String listToPrintString = IntStream.range(1, 100).boxed()
+                .map(FizzBuzz::convert)
+                .collect(Collectors.joining(" "));
+        System.out.print(listToPrintString);
+    }
+}
---
+++ src/test/java/io/trepixxx/fizzbuzz/CheatSheetGeneratorTest.java
@@ -0,0 +1,38 @@
+package io.trepixxx.fizzbuzz;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CheatSheetGeneratorTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUpOutStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    void shouldPrintNumbersConvertedFrom1To100() {
+        CheatSheetGenerator.print();
+
+        String expectedPrintOfTheBeginning = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16";
+        String expectedPrintOfTheEnd = "89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz";
+
+        assertTrue(outContent.toString().startsWith(expectedPrintOfTheBeginning));
+        assertTrue(outContent.toString().endsWith(expectedPrintOfTheEnd));
+    }
+}
+
+
