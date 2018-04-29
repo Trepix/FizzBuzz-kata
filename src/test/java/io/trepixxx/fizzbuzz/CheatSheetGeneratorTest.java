2adcf2a95c9fff69a0b4cf426d8d0a339c715595
trepix <pau.trepat.segura@gmail.com>
2018-04-29 23:27:17 +0200

    e. Extend integration test to check Jazz permutations are printed

--- src/test/java/io/trepixxx/fizzbuzz/CheatSheetGeneratorTest.java
+++ src/test/java/io/trepixxx/fizzbuzz/CheatSheetGeneratorTest.java
@@ -28,11 +28,13 @@ class CheatSheetGeneratorTest {
         CheatSheetGenerator.print();
 
         String expectedPrintOfTheBeginning = "1 2 Fizz 4 Buzz Fizz Jazz 8 Fizz Buzz 11 Fizz 13 Jazz FizzBuzz 16";
+        String expectedPrintContainingJazzCombinations = "FizzBuzz 61 62 FizzJazz 64 Buzz Fizz 67 68 Fizz BuzzJazz";
         String expectedPrintOfTheEnd = "89 FizzBuzz Jazz 92 Fizz 94 Buzz Fizz 97 Jazz Fizz";
 
-        assertTrue(outContent.toString().startsWith(expectedPrintOfTheBeginning));
-        assertTrue(outContent.toString().endsWith(expectedPrintOfTheEnd));
+
+        String outString = outContent.toString();
+        assertTrue(outString.startsWith(expectedPrintOfTheBeginning));
+        assertTrue(outString.contains(expectedPrintContainingJazzCombinations));
+        assertTrue(outString.endsWith(expectedPrintOfTheEnd));
     }
 }
-
-
