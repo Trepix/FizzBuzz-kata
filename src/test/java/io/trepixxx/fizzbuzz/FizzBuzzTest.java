7bb3664e2019985abffd6af31e3f3955aba631f0
trepix <pau.trepat.segura@gmail.com>
2018-04-29 20:14:12 +0200

    a. Refactor tests to include Jazz concept
    Because new concept is added to our domain, "Jazz" numbers, we prefer to refactor our current test suite to make the new concepts and requirements explicit

--- src/test/java/io/trepixxx/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/io/trepixxx/fizzbuzz/FizzBuzzTest.java
@@ -8,29 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
-    @ValueSource(ints = {1, 4, 11, 58, 98})
+    @ValueSource(ints = {1, 4, 11, 58, 97})
     @ParameterizedTest(name = "number \"{0}\" is converted to {0}")
-    void givenNonFizzNorBuzzNumber_ThenReturnThisNumber(Integer number) {
+    void givenNonFizzNorBuzzNorJazzNumber_ThenReturnThisNumber(Integer number) {
         assertEquals(number.toString(), convert(number));
     }
 
 
     @ValueSource(ints = {3, 6, 33, 99})
     @ParameterizedTest(name = "number \"{0}\" is converted to Fizz")
-    void givenFizzButNotBuzzNumber_ThenReturnFizz(Integer number) {
+    void givenFizzButNotBuzzNorJazzNumber_ThenReturnFizz(Integer number) {
         assertEquals("Fizz", convert(number));
     }
 
-
     @ValueSource(ints = {5, 10, 50, 95})
     @ParameterizedTest(name = "number \"{0}\" is converted to Buzz")
-    void givenBuzzButNotFizzNumber_ThenReturnBuzz(Integer number) {
+    void givenBuzzButNotFizzNorJazzNumber_ThenReturnBuzz(Integer number) {
         assertEquals("Buzz", convert(number));
     }
 
     @ValueSource(ints = {15, 30, 45, 90})
     @ParameterizedTest(name = "number \"{0}\" is converted to FizzBuzz")
-    void givenFizzBuzzNumber_ThenReturnFizzBuzz(Integer number) {
+    void givenFizzBuzzNumberButNotJazz_ThenReturnFizzBuzz(Integer number) {
         assertEquals("FizzBuzz", convert(number));
     }
 }
