de4fad5f97647ab27e202ac75db5b2329235d0be
trepix <pau.trepat.segura@gmail.com>
2018-05-01 03:51:07 +0200

    XI. Remove old tests coupled to domain logic ZzRules

--- src/test/java/io/trepixxx/fizzbuzz/ZzTranslatorTest.java
+++ src/test/java/io/trepixxx/fizzbuzz/ZzTranslatorTest.java
@@ -1,13 +1,7 @@
 package io.trepixxx.fizzbuzz;
 
-import io.trepixxx.fizzbuzz.rules.BuzzRule;
-import io.trepixxx.fizzbuzz.rules.FizzRule;
-import io.trepixxx.fizzbuzz.rules.JazzRule;
 import io.trepixxx.fizzbuzz.rules.ZzRule;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -16,16 +10,6 @@ import static org.mockito.Mockito.when;
 class ZzTranslatorTest {
 
     private static final Integer NUMBER = 5;
-    private ZzTranslator zzTranslator;
-
-    @BeforeEach
-    void instanceRule() {
-        zzTranslator = new ZzTranslator(
-                new FizzRule(),
-                new BuzzRule(),
-                new JazzRule()
-        );
-    }
 
     @Test
     void givenNoRules_ReturnNumberAsString() {
@@ -71,41 +55,5 @@ class ZzTranslatorTest {
 
         ZzTranslator zzTranslator = new ZzTranslator(abcMetRule, notMetRule, xyzMetRule);
         assertEquals(abcTranslation + xyzTranslation, zzTranslator.convert(NUMBER));
-    }
-
-    @ValueSource(ints = {3, 6, 33, 99})
-    @ParameterizedTest(name = "number \"{0}\" is converted to Fizz")
-    void givenFizzButNotBuzzNorJazzNumber_ThenReturnFizz(Integer number) {
-        assertEquals("Fizz", zzTranslator.convert(number));
-    }
-
-    @ValueSource(ints = {5, 10, 50, 95})
-    @ParameterizedTest(name = "number \"{0}\" is converted to Buzz")
-    void givenBuzzButNotFizzNorJazzNumber_ThenReturnBuzz(Integer number) {
-        assertEquals("Buzz", zzTranslator.convert(number));
-    }
-
-    @ValueSource(ints = {7, 14, 49, 98})
-    @ParameterizedTest(name = "number \"{0}\" is converted to Jazz")
-    void givenJazzButNotFizzNorBuzzNumber_ThenReturnJazz(Integer number) {
-        assertEquals("Jazz", zzTranslator.convert(number));
-    }
-
-    @ValueSource(ints = {15, 30, 45, 90})
-    @ParameterizedTest(name = "number \"{0}\" is converted to FizzBuzz")
-    void givenFizzBuzzNumberButNotJazz_ThenReturnFizzBuzz(Integer number) {
-        assertEquals("FizzBuzz", zzTranslator.convert(number));
-    }
-
-    @ValueSource(ints = {21, 42, 63})
-    @ParameterizedTest(name = "number \"{0}\" is converted to FizzJazz")
-    void givenFizzJazzButNotBuzzNumber_ThenReturnFizzJazz(Integer number) {
-        assertEquals("FizzJazz", zzTranslator.convert(number));
-    }
-
-    @ValueSource(ints = {35, 70})
-    @ParameterizedTest(name = "number \"{0}\" is converted to BuzzJazz")
-    void givenBuzzJazzButNotFizzNumber_ThenReturnBuzzJazz(Integer number) {
-        assertEquals("BuzzJazz", zzTranslator.convert(number));
     }
 }
